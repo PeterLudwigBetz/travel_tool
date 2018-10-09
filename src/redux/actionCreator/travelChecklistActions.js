981fd28956d1d3e8bb8d2f27ea07ba4a597f67c9
Ike Njoku <ikeenjoku@gmail.com>
2018-10-09 11:49:33 +0100

    feat(checklist): create new files for checklists
    - create new actions, sagas and reducer for travel checklist
    
    [Starts #161044557]

---
+++ src/redux/actionCreator/travelChecklistActions.js
@@ -0,0 +1,23 @@
+import {
+  UPDATE_TRAVEL_CHECKLIST,
+  UPDATE_TRAVEL_CHECKLIST_SUCCESS,
+  UPDATE_TRAVEL_CHECKLIST_FAILURE,
+} from '../constants/actionTypes';
+
+export const updateTravelChecklist = (checklistItemId, checklistItemData) => ({
+  type: UPDATE_TRAVEL_CHECKLIST,
+  checklistItemId,
+  checklistItemData
+});
+
+export const updateChecklistSuccess = (updatedChecklistItem) => ({
+  type: UPDATE_TRAVEL_CHECKLIST_SUCCESS,
+  updatedChecklistItem,
+});
+
+export const updateChecklistFailure = (error) => ({
+  type: UPDATE_TRAVEL_CHECKLIST_FAILURE,
+  error,
+});
+
+
